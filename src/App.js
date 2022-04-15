5a5a505438abef085d74af880d250b92bbff9aa0
Shazi123 <103746902+Shazi123@users.noreply.github.com>
2022-04-15 23:50:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MainPage from './MainPage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function App() {
+  return (
+    <div className="App">
+     <MainPage/>
+    </div>
+  );
+}
+
+export default App;
